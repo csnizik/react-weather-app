d9f26f41cfd6626cf24e48549b82f18983dff551
csnizik <csnizik@tulane.edu>
2023-02-26 18:02:05 -0600

    rm unused

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-export const GEO_URL = 'http://api.openweathermap.org/geo/1.0/direct';
-export const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather'
-export const UNITS = 'imperial'
-
-
+export const API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY;
+export const API_BASE_URL = 'https://api.openweathermap.org';
+export const UNITS = 'imperial';
+export const GEO_URL = `${API_BASE_URL}/geo/1.0/direct?appid=${API_KEY}`;
+export const WEATHER_URL = `${API_BASE_URL}/data/2.5/weather?appid=${API_KEY}&units=${UNITS}`;
